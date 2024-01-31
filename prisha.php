448b0177625533192cc63b9510aa1def311ccb83
vipaniprisha <158015623+vipaniprisha@users.noreply.github.com>
2024-01-31 12:56:49 +0530

    Add files via upload

---
+++ prisha.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+<body>
+        <title>clock</title>
+		
+		time and temprature<br>
+		5 mode funcations<br>
+	        
+</body>		
+</html>
+
+<?php
+
+
+date_default_timezone_set('asia/kolkata');
+	$a=date ("d-m-y h:i:s");
+	//$b=time();
+	
+  
+  
+  echo $a."<br>";
+  //echo $b;
+  
+ ?> 
